4615699f2b4fa6904acff9d2ffcbe5b3d7cd12cb
Rosano Coutinho <pub@xeari.com>
2021-11-21 09:24:30 -0500

    Spec robots.txt allow all

--- os-app/tech-robots/controller.js
+++ os-app/tech-robots/controller.js
@@ -4,17 +4,7 @@ exports.OLSKControllerRoutes = function() {
 		OLSKRouteMethod: 'get',
 		OLSKRouteSignature: 'EASRobotsRoute',
 		OLSKRouteFunction (req, res, next) {
-			return res.send(require('OLSKRobots').OLSKRobotsTXT([
-				'EASVitrineRoute',
-				'EASGlanceRoute',
-				'EASProjectsJSONRoute',
-			].reduce(function (coll, item) {
-				return coll.concat(res.locals.OLSKCanonical(item)).concat((res.locals.OLSKRouteObjectFor(item).OLSKRouteLanguageCodes || []).map(function (e) {
-					return res.locals.OLSKCanonicalLocalized(item, {
-						OLSKRoutingLanguage: e,
-					});
-				}));
-			}, []).concat('/application_icon_*')));
+			return res.send('User-agent: *\nAllow: /\n');
 		},
 	}];
 };
--- os-app/tech-robots/ui-test-_misc.js
+++ os-app/tech-robots/ui-test-_misc.js
@@ -3,17 +3,7 @@ const kDefaultRoute = require('./controller.js').OLSKControllerRoutes().shift();
 describe('EASRobots_Misc', function () {
 
 	it('sends text', async function () {
-		browser.assert.deepEqual(await (await browser.fetch('http://localhost' + kDefaultRoute.OLSKRoutePath)).text(), require('OLSKRobots').OLSKRobotsTXT([
-			require('../open-vitrine/controller.js').OLSKControllerRoutes().shift(),
-			require('../open-glance/controller.js').OLSKControllerRoutes().shift(),
-			require('../api-projects/controller.js').OLSKControllerRoutes().pop(),
-		].reduce(function (coll, item) {
-			return coll.concat(OLSKTestingCanonical(item)).concat((item.OLSKRouteLanguageCodes || []).map(function (e) {
-				return OLSKTestingCanonical(item, {
-					OLSKRoutingLanguage: e,
-				});
-			}));
-		}, []).concat('/application_icon_*')));
+		browser.assert.deepEqual(await (await browser.fetch('http://localhost' + kDefaultRoute.OLSKRoutePath)).text(), 'User-agent: *\nAllow: /\n');
 	});
 
 });
