c46ed1d22df2b5d70a5c0589e39aa133c8d734a0
Rosano Coutinho <pub@xeari.com>
2022-10-18 12:12:51 +0200

    Purge OLSKJar

--- os-app/open-vitrine/ui-test-_access.js
+++ os-app/open-vitrine/ui-test-_access.js
@@ -62,10 +62,6 @@ describe('EASVitrine_Access', function () {
 		browser.assert.elements('.ROCOGazette', 1);
 	});
 
-	it('shows OLSKJar', function () {
-		browser.assert.elements('.OLSKJar', 1);
-	});
-
 	it('shows OLSKEdit', function () {
 		browser.assert.elements('.OLSKEdit', 1);
 	});
